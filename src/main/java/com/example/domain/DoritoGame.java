4399dd4e36fcc17e671fea059a78dc55ac4b34a7
brynglet <jbryngel@gmail.com>
2023-12-27 00:02:39 +0100

    Sätta fix dorito från start kommenterad

--- src/main/java/com/example/domain/DoritoGame.java
+++ src/main/java/com/example/domain/DoritoGame.java
@@ -43,6 +43,46 @@ public class DoritoGame {
     List<Integer> hard = List.of(24, 12, 8); // 1,2,3 trianlges
          */
         if (i % 2 != 0 && k % 2 != 0) {
+
+            /*
+            Sätta  en fix dorito, Skrotis svåra
+           k är kolumn, i ör rad!!??
+            if (i == 1 && k == 1) {
+                return 2;
+            }
+
+            if (k == 1 && i == 7) {
+                return 2;
+            }
+
+            if (k == 7 && i == 1) {
+                return 2;
+            }
+
+            if (k == 7 && i == 5) {
+                return 2;
+            }
+
+            if (k == 7 && i == 7) {
+                return 2;
+            }
+
+            if (k == 3 && i == 3) {
+                return 1;
+            }
+
+            if (k == 3 && i == 5) {
+                return 1;
+            }
+
+            if (k == 3 && i == 7) {
+                return 1;
+            }
+
+            if (true) return 0;
+             */
+
+
             int randNumber = (int) (Math.random() * 100); //0-99
 
             if (randNumber < 20) {
